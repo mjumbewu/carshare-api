7dd423e25be84b13978a5c05b499b7df331a9904
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-02-25 15:07:06 -0500

    Add zipcar locations

--- locations.py
+++ locations.py
@@ -13,7 +13,7 @@ def get_pcs_locations_js():
     return loc_js
 
 def get_pcs_locations():
-    pattern = 'pods.push\((\[.*\])\);\n'
+    pattern = r'pods.push\((\[.*\])\);' + '\n'
     matches = re.findall(pattern, get_pcs_locations_js())
 
     # Take advantage of the fact that JS lists have the same format as Pythong
@@ -22,5 +22,16 @@ def get_pcs_locations():
 
     return locations
 
+def get_zipcar_locations():
+    zipcar_loc_file = urlopen('http://www.zipcar.com/philadelphia/find-cars.js?zipfleet_id=68586539&van_only_p=0&corporate_plan_key=&v=1325596458')
+    zipcar_loc_data = zipcar_loc_file.read()
+
+    pattern = r'var locs = (\[.*\]);'
+    match = re.search(pattern, zipcar_loc_data)
+
+    locations = eval(match.group(1))
+
+    return locations
+
 if __name__ == '__main__':
-    print get_pcs_locations()
+    print get_zipcar_locations()
--- project.py
+++ project.py
@@ -3,10 +3,15 @@ import json
 app = Flask(__name__)
 
 from locations import get_pcs_locations
+from locations import get_zipcar_locations
 
 @app.route("/pcs_locations")
 def _():
     return json.dumps(get_pcs_locations())
+
+@app.route("/zipcar_locations")
+def _():
+    return json.dumps(get_zipcar_locations())
 
 if __name__ == "__main__":
     app.run()
